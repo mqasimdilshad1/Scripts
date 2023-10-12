28db013ee5d8f30fb12f6d052eb68a12c5507602
Muhammad Qasim Dilshad <qasimdilshad336@gmail.com>
2023-10-12 15:30:04 +0500

    Task6 Completed

---
+++ task6/task6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+border_char=""
+string_to_print=""
+
+while getopts ":s:c:" opt; do
+  case $opt in
+    s) string_to_print="$OPTARG" ;;
+    c) border_char="$OPTARG" ;;
+    \?) echo "Invalid option: -$OPTARG" >&2
+        exit 1 ;;
+  esac
+done
+
+string_length=${#string_to_print}
+
+if [ "$border_char" != "" ]; then
+  
+  border=""
+  for ((i=0; i<string_length+4; i++)); do
+      border="${border}${border_char}"
+  done
+
+
+  echo "$border"
+
+  echo "${border_char} $string_to_print ${border_char}"
+
+  echo "$border"
+else
+  echo "$string_to_print"
+fi
