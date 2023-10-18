9c2a4b3d0a0e15b272fe7b1d8bfd890ea45f030b
Muhammad Qasim Dilshad <qasimdilshad336@gmail.com>
2023-10-18 16:45:34 +0500

    variables and arguments modified

--- task1/task1.sh
+++ task1/task1.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-file="$1"
-folder="$2"
+file=$1
+folder=$2
 
-if [ -z "$file" ] || [ -z "$folder" ]; then
+if [ -z $file ] || [ -z $folder ]; then
   echo "give arguments for file searching: <file> <folder>"
   exit 1
 fi
 
 function search_recursive() {
-  local dir="$1"
+  local dir=$1
 
-  for entry in "$dir"/*; do
-    if [ -f "$entry" ]; then
-      if [ "$(basename "$entry")" = "$file" ]; then
+  for entry in $dir/*; do
+    if [ -f $entry ]; then
+      if [ $(basename $entry) = $file ]; then
         echo "Found $file in: $entry"
       fi
-    elif [ -d "$entry" ]; then
-      search_recursive "$entry"
+    elif [ -d $entry ]; then
+      search_recursive $entry
     fi
   done
 }
-search_recursive "$folder"
+search_recursive $folder
 echo "Search complete."
 
 
