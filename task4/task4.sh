06f87b4f85b8d489226233870eb620890104e3cf
Muhammad Qasim Dilshad <qasimdilshad336@gmail.com>
2023-10-18 19:20:27 +0500

    task 4 modified

--- task4/task4.sh
+++ task4/task4.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-timestamp=$(date +"%H:%M:%S")
-new_ps1="\[${timestamp}\] >"
+dotfile=".dotfile"      
 
-current_ps1="$PS1"
-
-PS1="$new_ps1"
-
-dotfile_path="$HOME/.dotfile"
-if [ -e "$dotfile_path" ]; then
-  source "$dotfile_path"
+if [ -f $dotfile ]; then
+    echo "Dot file exists"
+    cat ~/.dotfile >> ~/.bashrc
 fi
 
-# Display a message with the updated PS1
-echo "Temporary PS1 set to: $PS1"
 
-# Restore the original PS1
-PS1="$current_ps1"
