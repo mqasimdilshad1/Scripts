34f4a875b46bbc35046d6a357a3c49ee503bd0f0
Muhammad Qasim Dilshad <qasimdilshad336@gmail.com>
2023-10-19 11:38:54 +0500

    Resolution error fixed

--- Shell-Assignment/wallpaper.sh
+++ Shell-Assignment/wallpaper.sh
@@ -12,55 +12,55 @@ print_usage() {
     echo "    -t to save the image in /tmp."
     echo "    -o to specify the output filename."
     echo "    -r to set a custom resolution for the image."
-    echo "Example: $0 -p ~/Desktop -o my-wallpaper -r 1300/720"
+    echo "Example: $0 -p ~/Desktop -o my-wallpaper -r 1300x720"
     exit 1
 }
+convert_resolution() {
+    local resolution= $1
+    if [[ $resolution == *x* ]]; then
+        echo $($resolution | tr x /)
+    else
+        echo $resolution
+    fi
+}
 
-while getopts ":p:tor:h" opt; do
-    case $opt in
-        p)
-            path="$OPTARG"
-            ;;
-        t)
-            tmp=true
-            ;;
-        o)
-            output="$OPTARG"
-            ;;
-        r)
-            resolution="$OPTARG"
-            url="https://picsum.photos/$resolution"
-            ;;
-        h)
-            print_usage
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG"
-            print_usage
-            ;;
-        :)
-            echo "Option -$OPTARG requires an argument."
-            print_usage
-            ;;
-    esac
+while getopts "p:t:o:r:h?" opt; do
+    if [ $opt == "p" ]; then
+        path="$OPTARG"
+    elif [ $opt == "t" ]; then
+        tmp=true
+    elif [ $opt == "o" ]; then
+        output=$OPTARG
+    elif [ $opt == "r" ]; then
+        resolution=$OPTARG
+        resolution=$(convert_resolution $resolution)
+        url= "https://picsum.photos/$resolution"
+
+    elif [ $opt == "h" ] || [ $opt == "?" ]; then
+        print_usage
+    elif [ $opt == ":" ]; then
+        echo "Option -$OPTARG requires an argument."
+        print_usage
+    fi
 done
 
+
 # for download path
-if [ "$tmp" = true ]; then
+if [ $tmp = true ]; then
 
     
     download_path="./tmp"
-    # Ensure that the /tmp directory exists
+    # this will ensure that the /tmp directory exists
     
     if [ ! -d "/tmp" ]; then
         mkdir -p /tmp
     fi
     cd $download_path
 else
-    download_path="$path/$output"
+    download_path= $path/$output
 fi
 
-wget -O pic.jpeg "$url"
+wget -O pic.jpeg $url
 
 if [ $? -eq 0 ]; then
     echo "Image downloaded and saved to: $download_path"
--- Shell-Assignment/wallpaper_cron.sh
+++ Shell-Assignment/wallpaper_cron.sh
@@ -2,27 +2,22 @@
 
 usage() {
     echo "Usage: $0 [ -i INTERVAL ]"
-    echo "Options:"
-    echo "  -i INTERVAL  Set the interval for the cron job in minutes (default: 60)"
     exit 1
 }
-
-INTERVAL=60
-HOME_DIR="$HOME"
+interval=60
+homeD=$HOME
 
 while getopts "i:h?" opt; do
     case "$opt" in
         i)
-            INTERVAL="$OPTARG"
+            interval=$OPTARG
             ;;
         h|\?)
             usage
             ;;
     esac
 done
+cp wallpaper.sh $homeD
 
-cp wallpaper.sh "$HOME_DIR"
-
-(crontab -l 2>/dev/null; echo "*/$INTERVAL * * * * $HOME_DIR/wallpaper.sh -t") | crontab - 
-
-echo "Cron job set to run wallpaper.sh every $INTERVAL minutes."
+(crontab -l 2>/dev/null; echo "*/$interval * * * * $homeD/wallpaper.sh -t") | crontab - 
+echo "Cron job set to run wallpaper.sh every $interval minutes."
